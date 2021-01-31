9bc165eb210ee624b575e71fc9ba332f26fe89b2
Johann Medina <hannder92@gmail.com>
2021-01-31 13:12:42 -0500

    Fixes

--- src/main/java/com/nuvu/usermanagement/web/controller/UserController.java
+++ src/main/java/com/nuvu/usermanagement/web/controller/UserController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import com.nuvu.usermanagement.domain.service.UserService;
 import com.nuvu.usermanagement.domain.User;
-import io.swagger.models.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -63,8 +62,8 @@ public class UserController {
 	@PostMapping()
 	@ApiOperation(value = "Create user", authorizations = { @Authorization(value = "JWT") })
 	@ApiResponses({ @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 404, message = "") })
-	public ResponseEntity<Object> createUser(@Valid @RequestBody User User) {
-		return new ResponseEntity<>(userService.save(User),HttpStatus.CREATED);
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+		return new ResponseEntity<>(userService.save(user),HttpStatus.CREATED);
 	}
 
 	@ApiOperation(value = "Delete user by id", authorizations = { @Authorization(value = "JWT") })
